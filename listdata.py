99f86a610e25d0e4f48632f0ef73142f6148a5e7
imchyna <chynaim@gmail.com>
2018-06-14 11:33:46 +0100

    Create listdata.py
    to browse image from directories.

---
+++ listdata.py
@@ -0,0 +1,16 @@
+import os
+path_dir = os.path.abspath('../DataSet/CASIA-WebFace')
+def GetFileList(dir):
+    newDir = dir
+    if os.path.isfile(dir):
+        # fileList.append(dir.decode('gbk'))
+        filename = dir.split('/')[6]+'_'+dir.split('/')[7]
+        img = imread(dir)
+        imsave('../DataSet/CASIA/'+filename,img)
+    elif os.path.isdir(dir):
+        for s in os.listdir(dir):
+            newDir=os.path.join(dir,s)
+            GetFileList(newDir)
+
+
+list = GetFileList(path_dir)
